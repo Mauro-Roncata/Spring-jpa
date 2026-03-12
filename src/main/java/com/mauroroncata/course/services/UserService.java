4f147ba68714e1d5a4f1d6ee47856df6a33f2e47
Mauro-Roncata <mauroroncata@gmail.com>
2026-03-12 11:22:48 -0300

    feat: exception handling - delete

--- src/main/java/com/mauroroncata/course/services/UserService.java
+++ src/main/java/com/mauroroncata/course/services/UserService.java
@@ -2,10 +2,12 @@ package com.mauroroncata.course.services;
 
 import com.mauroroncata.course.entities.User;
 import com.mauroroncata.course.repositories.UserRepository;
+import com.mauroroncata.course.services.exceptions.DatabaseException;
 import com.mauroroncata.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+       try {
+           repository.deleteById(id);
+       } catch (EmptyResultDataAccessException e){
+           throw new ResourceNotFoundException(id);
+       } catch (DataIntegrityViolationException e) {
+           throw new DatabaseException(e.getMessage());
+       }
     }
 
     public User update(Long id, User obj){
