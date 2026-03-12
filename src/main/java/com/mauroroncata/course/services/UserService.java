6a515a1a5fb22d881e6ab9f202c2e56aa8f12495
Mauro-Roncata <mauroroncata@gmail.com>
2026-03-12 11:01:15 -0300

    feat: exception handling - findById

--- src/main/java/com/mauroroncata/course/services/UserService.java
+++ src/main/java/com/mauroroncata/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.mauroroncata.course.services;
 
 import com.mauroroncata.course.entities.User;
 import com.mauroroncata.course.repositories.UserRepository;
+import com.mauroroncata.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
